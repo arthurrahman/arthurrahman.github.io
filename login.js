807e27a1edc872b407c1b1f837a267de6c63f033
Arthur Rahman <Arthur Rahman>
2017-03-12 18:56:47 +0100

    uri changed to github

--- login.js
+++ login.js
@@ -22,3 +22,16 @@ function onAuthCallback() {
   var token = authInfo["access_token"];
   console.log(token);
 }
+
+function getAuthInfoFromUrl() {
+  if (window.location.hash) {
+    var authResponse = window.location.hash.substring(1);
+    var authInfo = JSON.parse(
+      '{' + authResponse.replace(/([^=]+)=([^&]+)&?/g, '"$1":"$2",').slice(0, -1) + '}',
+      function (key, value) { return key === "" ? value : decodeURIComponent(value); });
+    return authInfo;
+  }
+  else {
+    alert("failed to receive auth token");
+  }
+}
